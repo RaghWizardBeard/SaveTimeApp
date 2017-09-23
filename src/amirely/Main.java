46203535ee62e8d64050bd18a5ed66aee1ec8b26
manuel <mcastro0528@outlook.com>
2017-09-23 13:54:46 -0400

    created the data class Person

---
+++ src/amirely/Main.java
@@ -0,0 +1,10 @@
+package src.amirely;
+
+public class Main {
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("This is a commit test");
+        System.out.println("fuck this shit");
+        System.out.println("new test");
+    }
+}
