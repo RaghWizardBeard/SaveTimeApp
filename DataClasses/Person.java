47f437fcbdad73ff2a1ff471eab7ae8c9f8314db
manuel <mcastro0528@outlook.com>
2017-09-23 13:50:27 -0400

    fuck this shit

---
+++ DataClasses/Person.java
@@ -0,0 +1,164 @@
+package DataClasses;
+
+
+import com.sun.jndi.cosnaming.IiopUrl;
+
+import java.util.Date;
+
+abstract class Person
+{
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private int ID;
+    private Date date;
+    private Date dob;
+    private String email;
+    private String address;
+    private String city;
+    private String state;
+    private int zipCode;
+    private String country;
+    private int phoneNumber;
+
+    public Person(String firstName, String lastName, String userName, int ID, Date date, Date dob, String email, String address,
+                  String city, String state, int zipCode, String country, int phoneNumber)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.ID = ID;
+        this.date = date;
+        this.dob = dob;
+        this.email = email;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.country = country;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
+
+    public int getID()
+    {
+        return ID;
+    }
+
+    public void setID(int ID)
+    {
+        this.ID = ID;
+    }
+
+    public Date getDate()
+    {
+        return date;
+    }
+
+    public void setDate(Date date)
+    {
+        this.date = date;
+    }
+
+    public Date getDob()
+    {
+        return dob;
+    }
+
+    public void setDob(Date dob)
+    {
+        this.dob = dob;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
+
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
+
+    public String getState()
+    {
+        return state;
+    }
+
+    public void setState(String state)
+    {
+        this.state = state;
+    }
+
+    public int getZipCode()
+    {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode)
+    {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry()
+    {
+        return country;
+    }
+
+    public void setCountry(String country)
+    {
+        this.country = country;
+    }
+
+
+
+}
